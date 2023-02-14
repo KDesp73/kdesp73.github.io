7fc98e4923e7b904ae5456bb2a8fcf71019d39c0
Konstantinos Despoinidis <63654361+KDesp73@users.noreply.github.com>
2023-02-14 23:58:01 +0200

    Update projects.js

--- docs/data/projects.js
+++ docs/data/projects.js
@@ -14,13 +14,13 @@ const projects = [
   {
     title: "Swing-Themes-Lib",
     link: "https://github.com/KDesp73/Swing-Themes-Library",
-    img: "https://www.barmaje.com/images/java_swing_logo.png"
+    img: "https://user-images.githubusercontent.com/63654361/218871863-16ae4acc-36ab-4039-9907-198fc741d9ec.jpg"
   },
 
   {
     title: "SyncR",
     link: "https://github.com/KDesp73/SyncR",
-    img: "https://user-images.githubusercontent.com/63654361/218871863-16ae4acc-36ab-4039-9907-198fc741d9ec.jpg"
+    img: "https://user-images.githubusercontent.com/63654361/218872414-29d6ddc2-fdac-4157-bccb-b8b4b1fdaaa6.png"
   },
 
   {
